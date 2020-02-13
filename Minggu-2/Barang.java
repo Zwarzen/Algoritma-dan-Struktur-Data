b28c81ab503050d4213e379a7102b17a604d125b
Risang <56747783+Zwarzen@users.noreply.github.com>
2020-02-13 08:04:49 +0700

    Add files via upload

---
+++ Minggu-2/Barang.java
@@ -0,0 +1,22 @@
+public class Barang {
+	String namaBarang, jenisBarang	;
+	int stok, hargaSatuan;
+
+	void tampilBarang(){
+		System.out.println("Nama : " + namaBarang);
+		System.out.println("Jenis : " + jenisBarang);
+		System.out.println("Stok : " + stok);
+		System.out.println("Harga Satuan : " + hargaSatuan);
+	}
+	void tambahStok(int n){
+		stok = stok + n;
+	}
+
+	void kurangiStok(int n){
+		stok = stok - n;
+	}
+
+	int hitungHargaTotal(int jumlah){
+		return jumlah*hargaSatuan;
+	}
+}
---
+++ Minggu-2/jawabanPertanyaan.md
@@ -0,0 +1,6 @@
+# jawaban 2.3.3
+
+1. karakteristik Class dan Objek
+Class : diawali huruf kapital
+Objek : harus instanisasi di method main
+2.  
