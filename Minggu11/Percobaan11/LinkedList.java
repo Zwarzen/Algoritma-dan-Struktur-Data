58f4d7dc09abb6b44ac5469720e4c682cd2404b7
Risang <56747783+Zwarzen@users.noreply.github.com>
2020-04-16 12:07:47 +0700

    Add files via upload

---
+++ Minggu11/Percobaan11/LinkedList.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu11;
+
+
+/**
+ *
+ * @author R154NG
+ */
+public class LinkedList {
+    Node head;
+    int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item) {
+        head = new Node(item, head);
+        size++;
+    }
+
+    public void add(int item, int index) throws Exception {
+        if (index < 0 || index > size) {
+            throw new Exception("Nilai index diluar batas");
+        }
+        if (isEmpty() || index == 0) {
+            addFirst(item);
+        } else {
+            Node temp = head;
+            for (int i = 1; i < index; i++) {
+                temp = temp.next;
+            }
+            Node next = (temp == null) ? null : temp.next;
+            temp.next = new Node(item, next);
+        }
+        size++;
+    }
+
+    public void addLast(int item) {
+        if (isEmpty()) {
+            addFirst(item);
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(item, null);
+        }
+        size++;
+    }
+
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("LinkedList Kosong");
+        }
+        return head.data;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai index di luar batas");
+        }
+        if (isEmpty() || index == 0) {
+            removeFirst();
+        } else {
+            Node prev = head;
+            Node cur = head.next;
+            for (int i = 1; i < index; i++) {
+                prev = cur;
+                cur = prev.next;
+            }
+            prev.next = cur.next;
+            size--;
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        head = head.next;
+        size--;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while (temp != null) {
+                System.out.println(temp.data + "\t");
+                temp = temp.next;
+            }
+            System.out.println();
+        } else {
+            System.out.println("LinkedList kosong");
+        }
+    }
+}
